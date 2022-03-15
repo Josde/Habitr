52e237aeed7e67b5880245d3e6272c071c6f8b14
Josde <jorgecruz@usal.es>
2022-03-15 12:50:09 +0100

    Added flutter_svg dependency Added basic profile screen (very wip)

--- lib/data/classes/user.dart
+++ lib/data/classes/user.dart
@@ -5,19 +5,20 @@ class User {
   // Defaults for empty constructor
   var uuid = Uuid();
   String id = '';
-  String name = 'Jorge';
+  String name = 'User'; //TODO: Remove this, this is for debug.
   Country country = Country.worldWide;
   DateTime createdAt = DateTime.now();
   int xp = 0;
   int currentStreak = 0; //FIXME: Esto son IDs de las rachas, no la longitud, aunque de momento para desarrollar la interfaz lo voy a tomar como longitud
   int maxStreak = 0;
+  int friendCount = 0;
   bool isAdmin = false;
 
   User.empty() {
     this.id = uuid.v4();
   }
 
-  User(this.name, this.country, this.createdAt, this.xp, this.currentStreak, this.maxStreak, this.isAdmin) {
+  User(this.name, this.country, this.createdAt, this.xp, this.currentStreak, this.maxStreak, this. friendCount, this.isAdmin) {
     this.id = uuid.v4();
   }
   User.fromJson(Map<String,dynamic> json) {
--- lib/screens/users/profilescreen.dart
+++ lib/screens/users/profilescreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
-
+import 'package:jdenticon_dart/jdenticon_dart.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import '../../data/classes/user.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -15,10 +16,12 @@ class _ProfilescreenState extends State<ProfileScreen>
   late AnimationController _controller;
   var uuid = Uuid();
   User u = User.empty();
+  String? avatarSvg = '';
   @override
   void initState() {
     _controller = AnimationController(vsync: this);
     super.initState();
+    avatarSvg = Jdenticon.toSvg(u.name);
   }
 
   @override
@@ -42,10 +45,76 @@ class _ProfilescreenState extends State<ProfileScreen>
       body: Column(
         children: [
           Row( // User name and etc row
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-
+              SvgPicture.string(avatarSvg!, fit: BoxFit.contain, height: 128, width: 128),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('${u.name}'),
+                  Text('${u.country.displayName}'),
+                  Text('${u.xp}xp'), //TODO: Add rank when the functionality is done.
+                ],
+              )
             ],
-          )
+          ),
+          Divider(
+            color: Colors.white,
+          ),
+          IntrinsicHeight(
+            child: Row( //Stadistics and friends button
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded( // Stadistics
+                    child: GestureDetector(
+                      onTap: (){print('Tapped stadistics');},
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white12,
+                          border: Border.all(color: Colors.white24),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+                            child: Center(child: Text('Stadistics')),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded( // Friends
+                    child: GestureDetector(
+                      onTap: (){print('Tapped friends');},
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white12,
+                          border: Border.all(color: Colors.white24),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+                          child: Center(
+                            child: Column(
+                              children: [
+                                Text('Friends'),
+                                Text('${u.friendCount}')
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+            ),
+          ),
+          Divider(
+            color: Colors.white,
+          ),
+          Column( // TODO: Feed
+
+          ),
+          Spacer(),
+          Container(), // TODO: Visit button
         ],
       )
     );
